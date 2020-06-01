db4213b5a25002751eb1d46c349180cfdcbbb2ef
lukasjoc <45790416+lukasjoc@users.noreply.github.com>
2020-06-01 22:35:29 +0200

    gather all data to save

--- content.js
+++ content.js
@@ -1,61 +1,83 @@
 "use-strict"
 
-console.log("DEBUG!! Content Script is loaded...")
-
 function get_title() {
 	let title = document.querySelector("h1.title")
-	console.log(`TITLE: ${title.innerText}`)
-	return title.innerHTML
+	return title.innerText
 }
 
 function get_duration() {
 	let duration = document.querySelector(".ytp-time-duration")
-	console.log(`DURATION: ${duration.innerHTML}`)
-	return duration.innerHTML
+	return duration.innerText
 }
 
 function get_current() {
 	let current = document.querySelector(".ytp-time-current")
-	console.log(`CURRENT: ${current.innerHTML}`)
-	return current.innerHTML
+	return current.innerText
+}
+
+function get_id() {
+	let location = window.location
+	let s = location.search
+	return s.match(/[^=]{8,}/)[0]
+}
+
+function get_channel() {
+	let channel = document.querySelector(".ytd-channel-name")
+	return channel.innerText
+}
+
+function build_link() {
+	let href = window.location.href
+	let timestamp_s = calc_current_s()
+	let link = `${href}&t=${timestamp_s}`
+	return link
+}
+
+function calc_current_s() {
+	return to_secs(get_current()) 
+}
+
+function calc_duration_s() {
+	return to_secs(get_duration())
 }
 
 function calc_percent() {
-	duration_s = to_secs(get_duration())
-	current_s = to_secs(get_current())
-
-	return Math.round((current_s * 100) / duration_s)
+	return Math.round((calc_current_s() * 100) / calc_duration_s())
 }
 
 // Taken from: https://stackoverflow.com/questions/9640266/convert-hhmmss-string-to-seconds-only-in-javascript
 function to_secs(str) {
-    var p = str.split(':'),
-        s = 0, m = 1;
+	let p = str.split(":")
+	let s = 0
+	let m = 1
 
-    while (p.length > 0) {
-        s += m * parseInt(p.pop(), 10);
-        m *= 60;
-    }
+	while (p.length > 0) {
+  	s += m * parseInt(p.pop(), 10)
+    m *= 60
+  }
 
-    return s;
+	return s
 }
 
-// Start communicating with popup
+// This handles the communication request initiated by popup.js
 chrome.runtime.onMessage.addListener(communicate)
 function communicate(msg, sender, send_answer) {
-	console.log(msg)
-
-	if ((msg.from === "popup") && (msg.why === "add_video") ) {
-		console.log("IN THE CHECK DONE!!")
-		var payload = {
-			title: get_title(),
-			duration: get_duration(),
-			current: get_current(),
-			percent: calc_percent(),
-		}
-	
-		send_answer(payload)
-		return true
+	if ( (msg.from !== "popup") || (msg.why !== "add_video") ) {
+		return false
 	}
+	
+	let payload = {
+		id: get_id(),
+		channel: get_channel(),
+		link: build_link(),
+		title: get_title(),
+		duration: get_duration(),
+		current: get_current(),
+		percent: calc_percent()
+	}
+	console.log("DEBUG!!: PAYLOAD BEFORE SEND", payload)
+	send_answer(payload)
+
+	return true
 }
 
--- popup.js
+++ popup.js
@@ -1,45 +1,61 @@
 "use-strict"
 
-console.log("DEBUG!! Popup Script loaded...")
+// What to store and how?
+// Using the IndexedDB.. the data structure is designed around objects and could look
+// like ths. that way a search for data mutation or deletion is easy with id search. Id is always unique
+// ytsfl_db = {
+// 	1 ...
+//
+// 	{
+// 		"video_id": "VIDEO_ID",
+// 		"title": "SOME_TITLE",
+// 		"channel": "SOME_CHANNEL_NAME",
+// 		"duration": 7:28,
+// 		"current": 3:14,
+// 		"percent": 50,
+// 		"link": https://youtube.com/?v=[VIDEO_ID]&t=[current_s],
+// 	},
+//
+// 	... N
+// }
+//
 
-// Add Button
+
+// SETUP INDEXDB
+
+
+
 let add_btn = document.querySelector(".add")
 add_btn.addEventListener("click", handle_add)
 
-const catch_info = (info) => {
-	tray_emty = document.querySelector(".tray-emty")
-	tray_emty.style.display = "none"
+// take data from content script and save it to indexedDB
+// TODO: check on add if item in
+const add = (info) => {
+	console.log(info)
 
-	video = document.querySelector(".video")
-	video.style.display = "block"
-	
-	title = document.querySelector(".title")
-	title.innerHTML = info.title
 
-	current = document.querySelector(".current")
-	current.innerHTML = info.current
-
-	duration = document.querySelector(".duration")
-	duration.innerHTML = info.duration
-
-	current_percent = document.querySelector(".current-percent")
-	current_percent.innerHTML = info.percent
 }
 
-// TODO: Communicate with contentent and return with data
+// on button click communicate with content script to get current video data
+// then pipe the data to named function and then to localstorage. UNIT: YSFL
 function handle_add() {
-	console.log("add is clicked!")
-	let params = {active: true, currentWindow: true}
-	chrome.tabs.query(params, communicate)
 	
+	const params = {
+		active: true,
+		currentWindow: true
+	}
+
+	chrome.tabs.query(params, communicate)
 	function communicate(tabs) {
 		let payload =  {
 			from: "popup",
 			why: "add_video"
 		}
 	
-		chrome.tabs.sendMessage(tabs[0].id, payload, catch_info)
+		chrome.tabs.sendMessage(tabs[0].id, payload, add)
 	}
 }
 
 
+function render_list() {
+}
