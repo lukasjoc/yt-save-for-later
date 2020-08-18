dfa36fc034cbc79f2326db7e965eaddc5a0acb93
lukasjoc <jochamlu@gmail.com>
2020-08-19 01:05:52 +0200

    update with delete and make stuff to worl

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
 	<div id="app">
-		<div v-for="(i, index) in data" :key="index">
-			{{i}}
+		<div class="pure-g popup">
+			<button type="button" @click="addVideo()">Add</button>
+			<div v-for="(video, index) in data" :key="index">
+				<div class="pure-u-1-3">
+					<h3>{{video.title}}</h3>
+					<span>{{video.current}}</span> /
+					<span>{{video.duration}}</span>
+					<span>{{video.percent}} % consumed</span>
+				</div>
+				<div>
+					<a :href="video.link"><button type="button" class="resume">Resume</button></a>
+					<button type="button" class="delete" @click="deleteVideo(video.id)">Delete</button>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -9,26 +21,50 @@
 <script>
 module.exports = {
 	name: "App",
+	created() {
+		if(localStorage.videos) {
+			this.data = JSON.parse(localStorage.videos)
+		}
+	},
 	data: () => {
 		return {
-			msg: "Hello World. Shit this works : )",
-			data: [
-				{"test1": 1},
-				{"test2": 2},
-				{"test3": 3},
-				{"test4": 4}
-			]
-		}
-	}
-}
+			data: [],
+		};
+	},
+	methods: {
+		addVideo() {
+			const props = {
+				currentWindow: true,
+				active: true,
+			}
+			chrome.tabs.query(props, (tabs) => {
+				const payload = {
+					from: "popup",
+					why: "add_video",
+				}
+				chrome.tabs.sendMessage(tabs[0].id, payload, (res) => {
+					this.data.push(res)
+					localStorage.setItem("videos", JSON.stringify(this.data))
+				});
+			});
+		},
+		deleteVideo(id) {
+			let videos = JSON.parse(localStorage.videos)
+			for(let i = 0; i <= videos.length; i++) {
+				if(videos[i].id === id) {
+					videos.splice(i, 1)
+				}
+			} 
+		},
+	},
+};
 </script>
 
-<style lang="css" scoped>
-* {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
+
+<style scoped>
+/* @import url(https://unpkg.com/purecss@2.0.3/build/pure-min.css); */
+.popup {
+	width: 300px;
+	height: 300px;
 }
-</style
-
-
+</style>
--- src/content.js
+++ src/content.js
@@ -1,44 +1,76 @@
 "use-strict"
 
-function time_parse(str) {
-	let time_props = {
-		p: str.split(':'),
-		s: 0,
-		m: 1
-	}
-	while (time_props.p.length > 0) {
-		time_props.s += time_props.m * parseInt(time_props.p.pop(), 10)
-		time_props.m *= 60
-	}
-	return time_props.s
+function get_title() {
+	let title = document.querySelector("h1.title")
+	return title.innerText
 }
 
-const	id = window.location.search.match(/[^=]{8,}/)[0];
+function get_duration() {
+	let duration = document.querySelector(".ytp-time-duration")
+	return duration.innerText
+}
 
-const title = document.querySelector("h1.title").innerText;
+function get_current() {
+	let current = document.querySelector(".ytp-time-current")
+	return current.innerText
+}
 
-const channel = document.querySelector(".ytd-channel-name").innerText;
+function get_id() {
+	let location = window.location
+	let s = location.search
+	return s.match(/[^=]{8,}/)[0]
+}
 
-const duration = document.querySelector(".ytp-time-duration").innerText;
+function get_channel() {
+	let channel = document.querySelector(".ytd-channel-name")
+	return channel.innerText
+}
 
-const current = document.querySelector(".ytp-time-current").innerText;
+function build_link() {
+	let href = window.location.href
+	let timestamp_s = calc_current_s()
+	let link = `${href}&t=${timestamp_s}`
+	return link
+}
 
-const link = `${window.locaton.href}&t=${current}`;
+function calc_current_s() {
+	return to_secs(get_current()) 
+}
 
-const percent = Math.round((time_parse(current)* 100) / time_parse(duration));
+function calc_duration_s() {
+	return to_secs(get_duration())
+}
+
+function calc_percent() {
+	return Math.round((calc_current_s() * 100) / calc_duration_s())
+}
+
+function to_secs(str) {
+	let p = str.split(":")
+	let s = 0
+	let m = 1
+
+	while (p.length > 0) {
+  	s += m * parseInt(p.pop(), 10)
+    m *= 60
+  }
+
+	return s
+}
 
 chrome.runtime.onMessage.addListener(communicate)
 function communicate(msg, sender, send_answer) {
 	if ( (msg.from !== "popup") || (msg.why !== "add_video") ) return false
 	let payload = {
-		id: id,
-		title: title,
-		channel: channel,
-		duration: duration,
-		current: current,
-		link: link,
-		percent: percent
+		id: get_id(),
+		channel: get_channel(),
+		link: build_link(),
+		title: get_title(),
+		duration: get_duration(),
+		current: get_current(),
+		percent: calc_percent()
 	}
 	send_answer(payload)
+	console.log(payload);
 	return true
 }
