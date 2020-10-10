fe3518600b08b361a089add392b469a5d104c218
lukasjoc <jochamlu@gmail.com>
2020-10-10 20:13:55 +0200

    update to functional style and update versions.. vue3 doesnt work with parcel yet sadly

--- src/content.js
+++ src/content.js
@@ -1,76 +1,45 @@
 "use-strict"
 
-class Content {
+const getTitle = () => document.querySelector("h1.title").innerText
 
-	getTitle() {
-		let title = document.querySelector("h1.title")
-		return title.innerText
+const getDuration = () => document.querySelector(".ytp-time-duration").innerText
+
+const getCurrent = () => document.querySelector(".ytp-time-current").innerText
+
+const getvID = () => window.location.search.match(/[^=]{8,}/)[0]
+
+const getChannel = () => document.querySelector(".ytd-channel-name > yt-formatted-string").innerText
+
+const buildLink = () => `${window.location.href}&t=${calcCurrentS}`
+
+const calcCurrentS = () =>  toSeconds(getCurrent())
+
+const calcDurationS = () => toSeconds(getDuration())
+
+const calcPercent = () => Math.round((calcCurrentS() * 100) / calcDurationS())
+
+const toSeconds = (str) => {
+	let p = str.split(":")
+	let s = 0
+	let m = 1
+	while (p.length > 0) {
+		s += m * parseInt(p.pop(), 10)
+		m *= 60
 	}
-
-	getDuration() {
-		let duration = document.querySelector(".ytp-time-duration")
-		return duration.innerText
-	}
-
-	getCurrent() {
-		let current = document.querySelector(".ytp-time-current")
-		return current.innerText
-	}
-
-	getvID() {
-		let location = window.location
-		let s = location.search
-		return s.match(/[^=]{8,}/)[0]
-	}
-
-	getChannel() {
-		let channel = document.querySelector(".ytd-channel-name > yt-formatted-string")
-		return channel.innerText
-	}
-
-	buildLink() {
-		let h = window.location.href
-		let t = this.calcCurrentS()
-		return `${h}&t=${t}`
-	}
-
-	calcCurrentS() {
-		return this.toSeconds(this.getCurrent())
-	}
-
-	calcDurationS() {
-		return this.toSeconds(this.getDuration())
-	}
-
-	calcPercent() {
-		return Math.round((this.calcCurrentS()*100)/this.calcDurationS())
-	}
-
-	toSeconds(str) {
-		let p = str.split(":")
-		let s = 0
-		let m = 1
-		while (p.length > 0) {
-			s += m*parseInt(p.pop(), 10)
-			m *= 60
-		}
-		return s
-	}
-
+	return s
 }
 
-window.addEventListener("load", () =>  {
+window.addEventListener("load", () => {
 	chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 		if (msg.from === "POPUP_HTML" && msg.why === "ADD_VIDEO") {
-			const content = new Content()
 			let payload = {
-				vID: content.getvID(),
-				channel: content.getChannel(),
-				link: content.buildLink(),
-				title: content.getTitle(),
-				duration: content.getDuration(),
-				current: content.getCurrent(),
-				percent: content.calcPercent()
+				vID: getvID(),
+				channel: getChannel(),
+				link: buildLink(),
+				title: getTitle(),
+				duration: getDuration(),
+				current: getCurrent(),
+				percent: calcPercent()
 			}
 			sendResponse(payload)
 			return true
